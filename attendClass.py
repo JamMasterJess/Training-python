3f7e5f173b7091f5b13f3d29c2a1513d08fe9203
JamMasterJess <jesse.wendt26@yahoo.com>
2023-03-01 14:19:20 -0800

    changes

--- attendClass.py
+++ attendClass.py
@@ -2,4 +2,8 @@ class1 = input('Is there class today?')
 
 if class1 == 'y':
     print('Did you wake up on time?')
+      if class2 == 'y':
+      print('Get Weather')
+        if weather1 == 'y'
+        print('Is it nice out?')
 
