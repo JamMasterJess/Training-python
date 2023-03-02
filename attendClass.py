34ac73f491df5ac68832c06c591965fc3f64161e
JamMasterJess <jesse.wendt26@yahoo.com>
2023-03-02 12:00:20 -0800

    changes

--- attendClass.py
+++ attendClass.py
@@ -1,11 +1,20 @@
 class1 = input('Is there class today?')
-class2 = input('Get Weather')
+noclass1= input('Go back to sleep')
+class2 = input('20 Degrees')
 weather1 = input('Is it nice out?')
+weather2 = input('Do you have transportation?')
+class3 = input('GO TO CLASS!')
 
 if class1 == 'y':
   print('Did you wake up on time?')
-  if class2 == 'y':
-    print('Get Weather')
-    if weather1 == 'y':
-      print('Is it nice out?')
+elif noclass1 == 'n': 
+  print('Go back to sleep')
+if class2 == 'y':
+  print('20 Degrees')
+if weather1 == 'y':
+   print('Is it nice out?')
+if weather2 == 'y':
+  print('Do you have transportation?')  
+if class3 == 'y':
+  print('GO TO CLASS!')
 
