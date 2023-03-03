f8c30b3e3b7c2649ba30667907bf5840cfb7fcee
JamMasterJess <jesse.wendt26@yahoo.com>
2023-03-03 11:09:52 -0800

    Completed Flow chart script

--- attendClass.py
+++ attendClass.py
@@ -3,31 +3,28 @@ if class1 == 'y':
   wake1 = input('Did you wake up on time?')
   if wake1 == 'y':
     print('Get Weather')
+    weather1 = input('Is it nice out?')
+    if weather1 == 'y':
+      trans1 = input('Do you have transportation?')
+      if trans1 == 'y':
+        print('GO TO CLASS!')
+      if trans1 == 'n':
+        int2 = input('Do you have internet access?')
+        if int2 == 'y':
+          print('Attend the class live stream')
+        if int2 == 'n':
+          print('Go back to sleep')  
+    if weather1 == 'n':
+      int3 = input('Do you have internet access?')
+      if int3 == 'y':
+        print('Attend the class live stream')
+      if int3 == 'n':
+        print('Go back to sleep')
   if wake1 == 'n':
-    print('Do you have internet access?')
     int1 = input('Do you have internet access?')
     if int1 == 'y':
       print('Attend the Class live stream')
     if int1 == 'n':
       print('Go back to sleep') 
-  weather1 = input('Nice out?')
-  if weather1 == 'y':
-    trans1 = input('Do you have transportation?')
-    if trans1 == 'y':
-      print('GO TO CLASS!')
-    if trans1 == 'n':
-      print('Do you have internet access?') 
-      int2 = input('Do you have internet access?')
-      if int2 == 'y':
-        print('Attend the class live stream')
-      if int2 == 'n':
-        print('Go back to sleep')  
-  if weather1 == 'n':
-    print('Do you have internet access?')
-    int3 = input('Do you have internet access?') 
-    if int3 == 'y':
-      print('Attend the class live stream') 
-    if int3 == 'n':
-      print('Go back to sleep')  
 if class1 == 'n':
   print('Go back to sleep')
